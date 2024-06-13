b89caa62737a51c3d5c67b0d92ded515f40de663
Patrick Gross <patrick@pgross.de>
2024-06-13 13:04:19 +0200

    added gnome config

--- modules/default.nix
+++ modules/default.nix
@@ -3,5 +3,6 @@
     [(import ../modules/dev/git)]
     ++ [(import ../modules/dev/neovim)]
     ++ [(import ../modules/dev/tmux)]
-    ++ [(import ../modules/dev/zsh)];
+    ++ [(import ../modules/dev/zsh)]
+    ++ [(import ../modules/gnome)];
 }
---
+++ modules/gnome/default.nix
@@ -0,0 +1,11 @@
+{
+  pkgs,
+  ...
+}:{
+  programs.gnome-shell = {
+    enable = true;
+    extensions = [
+      { package = pkgs.gnomeExtensions.dash-to-panel; }
+    ];
+  };
+}
