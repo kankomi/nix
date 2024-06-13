f62c57f7170d58a008bc4eb96239e9976b6935b4
Patrick Gross <patrick@pgross.de>
2024-06-13 10:45:29 +0200

    added git aliases

--- modules/dev/git/default.nix
+++ modules/dev/git/default.nix
@@ -6,6 +6,12 @@
     enable = true;
     userName = "Patrick Gross";
     userEmail = "patrick@pgross.de";
+    aliases = {
+      a = "add";
+      ci = "commit";
+      co = "checkout";
+      st = "status";
+    };
     extraConfig = {
       init = { defaultBranch = "main"; };
       pull = { rebase = true; };
