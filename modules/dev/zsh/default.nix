914ed5ac8d9433837b427e9a1b3a406d21b73c43
Patrick Gross <patrick@pgross.de>
2024-06-13 12:12:00 +0200

    theme

--- modules/dev/zsh/default.nix
+++ modules/dev/zsh/default.nix
@@ -7,6 +7,7 @@
    zsh = {
       enable = true;
       enableCompletion = true;
+      autosuggestions.enable = true;
       shellAliases = {
         tm = "tmux";
         v = "nvim";
@@ -14,9 +15,13 @@
         ns = "sudo nixos-rebuild switch --flake ~/nix-setup#default";
         nt = "sudo nixos-rebuild test --flake ~/nix-setup#default";
       };
+      shellInit = ''
+      eval "$(oh-my-posh init zsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/catppuccin.omp.json)"
+      '';
    };
    oh-my-posh = {
        enable = true;
+
      };
    };
 }
