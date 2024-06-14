30b9c8222437e7c2225aaff79ccf0e423efc694a
Patrick Gross <patrick@pgross.de>
2024-06-14 13:00:07 +0200

    added wayland config

--- modules/default.nix
+++ modules/default.nix
@@ -4,5 +4,6 @@
     ++ [(import ../modules/dev/neovim)]
     ++ [(import ../modules/dev/tmux)]
     ++ [(import ../modules/dev/zsh)]
+    ++ [(import ../modules/wayland)]
     ++ [(import ../modules/gnome)];
 }
