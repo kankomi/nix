d9209fd422928ed3c7a431dff136ee7cda1f11fd
Patrick Gross <patrick@pgross.de>
2025-06-22 15:16:27 +0200

    added git credential helper

--- modules/dev/git/default.nix
+++ modules/dev/git/default.nix
@@ -1,7 +1,13 @@
-{ config
-, pkgs
-, ...
+{
+  config,
+  pkgs,
+  ...
 }: {
+  # Install the credential helper
+  home.packages = [
+    pkgs.git-credential-manager
+  ];
+
   programs.git = {
     enable = true;
     userName = "Patrick Gross";
@@ -12,11 +18,13 @@
       co = "checkout";
       st = "status";
     };
+    package = pkgs.gitFull; # or pkgs.git with pkgs.git-credential-libsecret if desired
     extraConfig = {
-      init = { defaultBranch = "main"; };
-      pull = { rebase = true; };
-      push = { autoSetupRemote = true; };
-      core = { whitespace = "trailing-space,space-before-tab"; };
+      init = {defaultBranch = "main";};
+      pull = {rebase = true;};
+      push = {autoSetupRemote = true;};
+      core = {whitespace = "trailing-space,space-before-tab";};
+      credential.helper = "libsecret";
     };
   };
 }
