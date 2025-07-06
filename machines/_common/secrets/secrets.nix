a0db6e09e34e521488142d502ff9b7a36a9d20c2
Patrick Gross <patrick@pgross.de>
2025-07-06 12:20:14 +0200

    updated secrets

--- machines/_common/secrets/secrets.nix
+++ machines/_common/secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDlS6/r/o4svSuAod+zU8iDMjCk/hPJRtXS9gULlFKLz";
-  laptop = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCo7Nw4G2IeeMuZqmv2UFeWnQPj4KbWCSCvivz2MsPb8hunw6HqVpRns5/ewrSwRvmQ5cLS0MLsrBVMWQJxjBR9cRGqzAXMx1aKvvsmd8fn8k/I6Ln5Im3GopFt30iZ3KrNWr+00oXQnma7I8GnxGPju9TjG/h3j2Y5MggDRDFdyRqLesPmhZRsEgTIHgrH0nfm6SEVuEAJRx4im04yIeO6p/QndGfQrCGNWNtXx7uZPkqZdjtJQ3hnoY1Y6Ww6Gguvcylp0PnWm+mB+R2Fl/2C4rYqBYVRBlQzFRuwj0/SwuzZj1j6wUDLuHxYvt/M9xiLk62cTHtHdXVBsiWNr1fiRM9sR72gxeIPhY6qtkGxhUSni4/2+UWoS8pZc2Ry6aIU7j7aZKA7eEmjdPP2iNOmjF/l7BTUl5WS7bu/awLI24ACkWx+G41yx7dxr4nWvdTfI6PNIVFA8HkEV+xBVIofAjAoWLYa4/QFxC0YKgkwkWNYckDyFFYumsFsSwd8fUE= grop@laptop";
+  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIyc4x14LKYJmV8pD8Zy+8ecCIdecy1oDS5kUUSgG8bT";
 in
 {
   "samba.age".publicKeys = [ vm ];
