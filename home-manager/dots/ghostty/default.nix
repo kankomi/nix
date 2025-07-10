bb1a099ad8cb8740a81a40db37a8e56c6cd1c1ca
Patrick Gross <patrick@pgross.de>
2025-07-10 15:27:45 +0200

    .

---
+++ home-manager/dots/ghostty/default.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      theme = "catppuccin-mocha";
+      font-family = "CascadiaCove Nerd Font";
+      background-opacity = 0.9;
+    };
+  };
+}
