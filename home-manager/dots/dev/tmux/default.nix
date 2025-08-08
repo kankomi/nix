490f3de47dac88c23d98ae5f4a131ef0f10f2dd2
Gross Patrick (VM/MFD2) <Patrick.Gross@de.bosch.com>
2025-08-08 09:56:42 +0200

    added tmuxinator

--- home-manager/dots/dev/tmux/default.nix
+++ home-manager/dots/dev/tmux/default.nix
@@ -4,64 +4,69 @@
   ...
 }:
 {
+  home.packages = with pkgs; [
+    tmuxinator
+  ];
+
   programs.tmux = {
     enable = true;
     plugins = with pkgs; [
       tmuxPlugins.catppuccin
       tmuxPlugins.vim-tmux-navigator
+      tmuxPlugins.resurrect
+
     ];
-    extraConfig =
-      ''
-        unbind C-a
-        set -g prefix C-a
-        set -g mouse on
+    extraConfig = ''
+      unbind C-a
+      set -g prefix C-a
+      set -g mouse on
 
-        set -g base-index 1
-        set -g pane-base-index 1
-        set -g status-style bg=default
+      set -g base-index 1
+      set -g pane-base-index 1
+      set -g status-style bg=default
 
-        set -g default-terminal "tmux-256color"
-        set -ag terminal-overrides ",xterm-256color:RGB"
+      set -g default-terminal "tmux-256color"
+      set -ag terminal-overrides ",xterm-256color:RGB"
 
-        set -g @catppuccin_window_status_style "rounded"
-        set -g status-left ""
-        set -g status-right "#{E:@catppuccin_status_application} #{E:@catppuccin_status_session}"
+      set -g @catppuccin_window_status_style "rounded"
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application} #{E:@catppuccin_status_session}"
 
-        # Active pane border styling
-        set -g pane-border-style 'fg=colour235'
-        set -g pane-active-border-style 'fg=colour148 bg=default'
-        set -g pane-border-lines heavy
-        set -g pane-border-status top
-        set -g pane-border-format " #{?pane_active,#[fg=colour148]● ,  }#P: #{pane_current_command} "
+      # Active pane border styling
+      set -g pane-border-style 'fg=colour235'
+      set -g pane-active-border-style 'fg=colour148 bg=default'
+      set -g pane-border-lines heavy
+      set -g pane-border-status top
+      set -g pane-border-format " #{?pane_active,#[fg=colour148]● ,  }#P: #{pane_current_command} "
 
-        set-option -s set-clipboard off
+      set-option -s set-clipboard off
 
-        set-option -g status-position top
-        # vim mode
-        set-window-option -g mode-keys vi
+      set-option -g status-position top
+      # vim mode
+      set-window-option -g mode-keys vi
 
-        bind-key h select-pane -L
-        bind-key j select-pane -D
-        bind-key k select-pane -U
-        bind-key l select-pane -R
+      bind-key h select-pane -L
+      bind-key j select-pane -D
+      bind-key k select-pane -U
+      bind-key l select-pane -R
 
-        # bindings
-        bind-key '"' split-window -c "#{pane_current_path}"
-        bind-key % split-window -h -c "#{pane_current_path}"
-        bind-key c new-window -c "#{pane_current_path}"
-      ''
-      + (
-        if builtins.match "^wsl.*" configName != null then
-          ''
-            bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "clip.exe"
-            bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "clip.exe"
-          ''
-        else
-          ''
-            bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "xclip -in -selection clipboard"
-            bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "xclip -in -selection clipboard"
-          ''
-      );
+      # bindings
+      bind-key '"' split-window -c "#{pane_current_path}"
+      bind-key % split-window -h -c "#{pane_current_path}"
+      bind-key c new-window -c "#{pane_current_path}"
+    ''
+    + (
+      if builtins.match "^wsl.*" configName != null then
+        ''
+          bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "clip.exe"
+          bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "clip.exe"
+        ''
+      else
+        ''
+          bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "xclip -in -selection clipboard"
+          bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "xclip -in -selection clipboard"
+        ''
+    );
 
   };
 }
--- home-manager/dots/zsh/default.nix
+++ home-manager/dots/zsh/default.nix
@@ -14,41 +14,41 @@
       enableCompletion = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
-      shellAliases =
-        {
-          tm = "tmux";
-          lg = "lazygit";
-          v = "nvim";
-          ls = "ls -h --color";
-          l = "ls -lh";
-          ll = "l";
-          la = "ls -la";
-          npm = "pnpm";
-        }
-        // (
-          if configName == "wsl_bosch" then
-            {
-              network-status = "sudo journalctl -u wsl-network.service -f | grep -v \"sudo\"";
-              px-status = "sudo journalctl -u px-proxy.service -f | grep -v \"sudo\"";
-              vpnkit-status = "sudo journalctl -u wsl-vpnkit.service -f | grep -v \"sudo\"";
-              network-restart = "sudo systemctl restart wsl-network.service";
-              px-restart = "sudo systemctl restart px-proxy.service";
-              vpnkit-restart = "sudo systemctl restart wsl-vpnkit.service";
-            }
-          else
-            { }
-        )
-        // (
-          if builtins.match "^wsl.*" configName != null then
-            {
-              hms = "home-manager switch --flake ~/nix#${configName}";
-            }
-          else
-            {
-              ns = "sudo nixos-rebuild switch --flake ~/nix#${configName}";
-              nt = "sudo nixos-rebuild test --flake ~/nix#${configName}";
-            }
-        );
+      shellAliases = {
+        tm = "tmux";
+        lg = "lazygit";
+        v = "nvim";
+        ls = "ls -h --color";
+        l = "ls -lh";
+        ll = "l";
+        la = "ls -la";
+        npm = "pnpm";
+        mux = "tmuxinator";
+      }
+      // (
+        if configName == "wsl_bosch" then
+          {
+            network-status = "sudo journalctl -u wsl-network.service -f | grep -v \"sudo\"";
+            px-status = "sudo journalctl -u px-proxy.service -f | grep -v \"sudo\"";
+            vpnkit-status = "sudo journalctl -u wsl-vpnkit.service -f | grep -v \"sudo\"";
+            network-restart = "sudo systemctl restart wsl-network.service";
+            px-restart = "sudo systemctl restart px-proxy.service";
+            vpnkit-restart = "sudo systemctl restart wsl-vpnkit.service";
+          }
+        else
+          { }
+      )
+      // (
+        if builtins.match "^wsl.*" configName != null then
+          {
+            hms = "home-manager switch --flake ~/nix#${configName}";
+          }
+        else
+          {
+            ns = "sudo nixos-rebuild switch --flake ~/nix#${configName}";
+            nt = "sudo nixos-rebuild test --flake ~/nix#${configName}";
+          }
+      );
 
       initContent =
         if configName == "wsl_bosch" then
